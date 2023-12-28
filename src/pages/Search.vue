8bfa3f9aed7d9ff3d0f71d0e5ba52c2d5af7f4f9
Amer Kulovic <amer.kulovic@yahoo.com>
2023-12-27 21:14:38 -0600

    added functionality to blog links

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -7,13 +7,15 @@ console.log(store.blogs);
 <template>
   <div class="flex flex-col items-center">
     <container v-for="(object, index) in store.blogs" :key="index" class="flex flex-row w-5/6 py-7 border-b border-slate-400">
-      <a :href="object.blog.urlToImage">
+      <a :href="object.blog.url" target="_blank">
         <img class="w-[263px] h-[175px]" :src="object.blog.imageURL" />
       </a>
       <section class="flex flex-col ml-5 w-4/5">
-        <p>{{ object.blog.source }}</p>
-        <h1 class="text-2xl font-bold font-serif">{{ object.blog.title }}</h1>
-        <p>By {{ object.blog.author }}</p>
+        <p class="font-extralight">{{ object.blog.source }}</p>
+        <a :href="object.blog.url" target="_blank">
+          <h1 class="text-2xl font-bold font-serif hover:underline">{{ object.blog.title }}</h1>
+        </a>
+        <p class="font-extralight">By {{ object.blog.author }}</p>
       </section>
     </container>
   </div>
