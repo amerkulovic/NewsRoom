1c36e8c7ed8a674e9ad9dcc34c85ccfe691ddffc
Amer Kulovic <amer.kulovic@yahoo.com>
2023-12-28 22:13:07 -0600

    added checks to clean up search experience

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import naImage from "../assets/na.png";
 import { useMyStore } from "../stores/store";
 
 const store = useMyStore();
@@ -8,14 +9,15 @@ console.log(store.blogs);
   <div class="flex flex-col items-center">
     <container v-for="(object, index) in store.blogs" :key="index" class="flex flex-row w-5/6 py-7 border-b border-slate-400">
       <a :href="object.blog.url" target="_blank">
-        <img class="w-[263px] h-[175px]" :src="object.blog.imageURL" />
+        <img v-if="object.blog.imageURL" class="w-[263px] h-[175px]" :src="object.blog.imageURL" />
+        <img v-else class="w-[263px] h-[175px]" :src="naImage" />
       </a>
       <section class="flex flex-col ml-5 w-4/5">
         <p class="font-extralight">{{ object.blog.source }}</p>
         <a :href="object.blog.url" target="_blank">
           <h1 class="text-2xl font-bold font-serif hover:underline">{{ object.blog.title }}</h1>
         </a>
-        <p class="font-extralight">By {{ object.blog.author }}</p>
+        <p v-if="object.blog.author" class="font-extralight">By {{ object.blog.author }}</p>
       </section>
     </container>
   </div>
