813ad4d6d1a3339ccd8c82d737d27a1eccb43b9b
Amer Kulovic <amer.kulovic@yahoo.com>
2024-01-22 11:52:31 -0600

    edited the Home text for mobile view

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@ import InfoBlock from "../components/InfoBlock.vue";
 <template>
   <div class="relative">
     <img :src="homePhoto" class="h-[600px] w-full object-cover" />
-    <h1 class="absolute flex text-end right-5 bottom-8 text-white font-extrabold text-4xl w-1/2 font-serif transform translateZ-10 drop-shadow-xl">Unveiling Truths, Igniting Minds: Your Source for Informed Perspectives and Breaking Stories.</h1>
+    <h1 class="absolute flex text-end right-5 bottom-8 text-white font-extrabold text-4xl w-1/2 font-serif transform translateZ-10 drop-shadow-xl max-md:text-2xl">Unveiling Truths, Igniting Minds: Your Source for Informed Perspectives and Breaking Stories.</h1>
   </div>
   <section class="flex items-center max-md:flex-col">
     <InfoBlock
