18e707758542d10fe36a0961a1333479cdaa3378
Amer Kulovic <amer.kulovic@yahoo.com>
2025-06-17 13:43:50 -0500

    removed comment

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import "./styles/main.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-/* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
