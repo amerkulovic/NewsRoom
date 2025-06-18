b290be78b6a1cffadb94c582097dfa3a6c3a0c10
Amer Kulovic <amer.kulovic@yahoo.com>
2025-06-18 18:30:07 -0500

    removed comment

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-/* import specific icons */
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMagnifyingGlass);
