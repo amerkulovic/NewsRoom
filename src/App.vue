8953474d3a43011a3d6606dc4d7bb9cb6876d9df
Amer Kulovic <amer.kulovic@yahoo.com>
2025-07-13 16:56:15 -0500

    fixed bug that did not import the Nav or the Footer correctly

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import * as Nav from "./components/Nav.vue";
-import * as Footer from "./components/Footer.vue";
+import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
 </script>
 <template>
   <Nav />
